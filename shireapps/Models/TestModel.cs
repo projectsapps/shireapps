d405be4b8a675db5fff5db5020ae1669a093865a
Elnie Gabato <egabato@aquip.io>
2019-11-15 07:11:57 +0000

    First commit

---
+++ shireapps/Models/TestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shireapps.Models
+{
+    public class TestModel
+    {
+        public int ID { get; set; }
+        public string Company { get; set; }
+        public string UnitNo { get; set; }
+        public string FluidLevels { get; set; }
+        public string Mileage { get; set; }
+ 
+    }
+}
